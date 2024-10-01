6c693ec822d6ebf772b6abe67f612f24d4a162b3
kdxiaoyi <kdxiaoyi@outlook.com>
2024-10-01 15:27:39 +0800

    重渲染脚本 v1.0.13[13] + 现在会在图片加载完成后重新计算contentBG的高度 + 增加图片出错处理

--- res/scripts/js/md-newUI-render.js
+++ res/scripts/js/md-newUI-render.js
@@ -26,7 +26,7 @@ const /*左侧边栏·第2格内容*/conf_sidebar_links=`
 <s-icon slot="start"><svg width="100px" height="100px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g><path fill="none" d="M0 0h24v24H0z"/><path d="M18.223 3.086a1.25 1.25 0 0 1 0 1.768L17.08 5.996h1.17A3.75 3.75 0 0 1 22 9.747v7.5a3.75 3.75 0 0 1-3.75 3.75H5.75A3.75 3.75 0 0 1 2 17.247v-7.5a3.75 3.75 0 0 1 3.75-3.75h1.166L5.775 4.855a1.25 1.25 0 1 1 1.767-1.768l2.652 2.652c.079.079.145.165.198.257h3.213c.053-.092.12-.18.199-.258l2.651-2.652a1.25 1.25 0 0 1 1.768 0zm.027 5.42H5.75a1.25 1.25 0 0 0-1.247 1.157l-.003.094v7.5c0 .659.51 1.199 1.157 1.246l.093.004h12.5a1.25 1.25 0 0 0 1.247-1.157l.003-.093v-7.5c0-.69-.56-1.25-1.25-1.25zm-10 2.5c.69 0 1.25.56 1.25 1.25v1.25a1.25 1.25 0 1 1-2.5 0v-1.25c0-.69.56-1.25 1.25-1.25zm7.5 0c.69 0 1.25.56 1.25 1.25v1.25a1.25 1.25 0 1 1-2.5 0v-1.25c0-.69.56-1.25 1.25-1.25z"/></g></svg></s-icon>
 Bilibili ↗</s-chip>`;
 const /*左侧边栏·第2格内容中没有按文档编写请启用此项*/conf_sidebar_links_preventDefault=false;
-const /*插件版本（建议不要修改）*/PluginVer=["1.0.12",12];
+const /*插件版本（建议不要修改）*/PluginVer=["1.0.13",13];
 
 //插入重渲染代码
 document.body.innerHTML = `
@@ -356,6 +356,8 @@ document.querySelectorAll('img').forEach((imgElement) => {
   if (imgElement.dataset.uiImg=="true") {return;};
   imgElement.classList.add("processed");
   if (conf_imgView_imgse) {imgElement.addEventListener("click",()=>{openImgView(imgElement.src.replace(/\.md\./,"."),imgElement.title);});} else {imgElement.addEventListener("click",()=>{openImgView(imgElement.src,imgElement.title);});};
+  imgElement.addEventListener("load",()=>{contentBG.style.height="initial";contentBG.style.height=`${contentBG.offsetHeight+appbar.offsetHeight}px`;console.output("修改页面真实高度\ncontentBG.style.height="+contentBG.style.height);});
+  imgElement.addEventListener("error",()=>{imgElement.removeEventListener("load",null);imgElement.removeEventListener("error",null);imgElement.src="https://rs.kdxiaoyi.top/res/images/load_err.svg";console.output("某个图片加载失败\nsrc="+imgElement.src);});
   console.output("向img添加了imgView绑定");
 });
 function openImgView(imgsrc, imgTitle) {
