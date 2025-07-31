19e6d9c1d2c95820793731bbf82301e9d75f8e37
kdxiaoyi [mipad7] <kdxiaoyi@outlook.com>
2025-07-31 03:22:32 +0000

    移除长时间不更新文件

--- res/scripts/js/main.js
+++
@@ -1,8 +0,0 @@
-// 跳转通用中间页
-
-function author_kdxiaoyi() {let a = document.createElement("a");a.setAttribute("href", 'https://github.com/kdxhub/');a.setAttribute("target", "_blank");a.click();a.remove();return;};
-function author_kdxiaoyi_bilibili() {let a = document.createElement("a");a.setAttribute("href", 'https://space.bilibili.com/1987247870');a.setAttribute("target", "_blank");a.click();a.remove();return;};
-function hub() {window.location.href="/"};
-console.log ("kdxiaoyi.top");console.log("Copyright (C) 2025, kdxiaoyi. All right reserved.\nSee more @ //rs.kdxiaoyi.top/licen");
-document.getElementById("noscript").setAttribute("style", "display:none;");
-const wait=3000; //等待时长（ms）
