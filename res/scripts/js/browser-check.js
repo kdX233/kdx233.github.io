c68af64caf5b6e9903185a750f31330ec7c7f864
kdxiaoyi [mipad7] <kdxiaoyi@outlook.com>
2025-08-30 21:04:41 +0800

    新增「可选链操作符」的测试，优化部分测试方法

--- res/scripts/js/browser-check.js
+++ res/scripts/js/browser-check.js
@@ -58,7 +58,7 @@ try {
 };
 // 安全上下文
 try {
-  log.output("安全上下文(Secure Context)", (self.isSecureContext ? "是：获取到安全上下文接口，当前是安全上下文。" : "否：获取到安全上下文接口，但当前不是是安全上下文。\n这通常是因为没有使用HTTPS协议访问本站点，建议切换到HTTPS协议导致的。本站已启用ForceHTTPS，目前可能配置故障。"));
+  log.output("安全上下文(Secure Context)", (self.isSecureContext ? "是：获取到安全上下文接口，当前是安全上下文。" : "否：获取到安全上下文接口，但当前不是是安全上下文。\n这通常是因为没有使用HTTPS协议访问本站点，建议切换到HTTPS协议导致的。本站已启用ForceHTTPS，目前可能配置故障。"), !self.isSecureContext);
 } catch (e) {
   log.output("安全上下文(Secure Context)", e, true);
 };
@@ -119,14 +119,14 @@ log.divider("JavaScript特性支持");
 // ()=>
 try {
   new Function("()=>{}");
-  log.output("箭头函数(Arrow Function)", "支持：成功构造了箭头函数");
+  log.output("箭头函数(Arrow Function)", "成功构造了箭头函数");
 } catch (e) {
   log.output("箭头函数(Arrow Function)", e, true);
 };
 // Fetch
 try {
   if (!self.fetch) { throw new Error("Fetch 未定义") };
-  log.output("Fetch API", "支持：获取到了Fetch API入口");
+  log.output("Fetch API", "获取到了Fetch API入口");
 } catch (e) {
   log.output("Fetch API", e, true);
 };
@@ -134,7 +134,7 @@ try {
 try {
   if (!self.Promise) { throw new Error("Promise 未定义") };
   new Function("return (async function(){ return true; })")();
-  log.output("异步函数", "支持：已通过Promise和async/await的检测");
+  log.output("异步函数", "已通过Promise和async/await的检测");
 } catch (e) {
   log.output("异步函数", e, true);
 };
@@ -146,7 +146,7 @@ try {
   const c = getComputedStyle(document.documentElement).getPropertyValue("--test-var");
   document.head.removeChild(s);
   if (c.trim() === "#000") {
-    log.output("样式表变量(CSS Variables)", "支持：已构造并校验了CSS变量");
+    log.output("样式表变量(CSS Variables)", "已构造并校验了CSS变量");
   } else {
     throw new Error(`构造的CSS变量应用再读取后无法校验：读取到的值为"${c.trim()}"，预期值为"#000"`);
   };
@@ -156,16 +156,23 @@ try {
 // 私有类方法
 try {
   new Function("class A { #p = 1; getP() { return this.#p; } }; return (new A()).getP()")();
-  log.output("私有类方法(Private Class Methods)", "支持：成功构造了包含私有方法的类");
+  log.output("私有类方法(Private Class Methods)", `成功构造了包含私有方法的类`);
 } catch (e) {
   log.output("私有类方法(Private Class Methods)", e, true);
 };
 // 模板字符串
 try {
   new Function("const a = 1; const b = 2; return `测试模板字符串：${a+b}`")();
-  log.output("模板字符串(Template String)", "支持：成功构造了模板字符串");
+  log.output("模板字符串(Template String)", "成功构造了模板字符串");
 } catch (e) {
   log.output("模板字符串(Template String)", e, true);
+};
+// 可选链操作符
+try {
+  new Function("const a = { b: { c: 1 } }; return a?.b?.c")();
+  log.output("可选链操作符(Optional Chaining)", `成功构造了可选链操作符`);
+} catch (e) {
+  log.output("可选链操作符(Optional Chaining)", e, true);
 };
 
 log.divider("浏览器API支持");
@@ -173,9 +180,9 @@ log.divider("浏览器API支持");
 try {
   document.cookie = "testcookie=1";
   const cookieEnabled = document.cookie.indexOf("testcookie=") != -1;
-  log.output("Cookie 支持(Cookie Enabled)", (cookieEnabled ? "是" : "否"), !cookieEnabled);
+  log.output("Cookie 支持(Cookie Enabled)", (cookieEnabled ? "操作成功完成" : "支持但已被禁用，这可能是因为本检测在file://协议头下运行"), !cookieEnabled);
 } catch (e) {
-  log.output("Cookie 支持(Cookie Enabled)", "否", true);
+  log.output("Cookie 支持(Cookie Enabled)", e, true);
 };
 // 本地存储支持
 try {
@@ -189,9 +196,9 @@ try {
       return false;
     };
   })();
-  log.output("本地存储支持(Local Storage)", (localStorageEnabled ? "是" : "否"), !localStorageEnabled);
+  log.output("本地存储支持(Local Storage)", (localStorageEnabled ? "操作成功完成" : "支持但已被禁用，请检查安全上下文"), !localStorageEnabled);
 } catch (e) {
-  log.output("本地存储支持(Local Storage)", "否", true);
+  log.output("本地存储支持(Local Storage)", e, true);
 };
 // 会话存储支持
 try {
@@ -205,7 +212,7 @@ try {
       return false;
     };
   })();
-  log.output("会话存储支持(Session Storage)", (sessionStorageEnabled ? "是" : "否"), !sessionStorageEnabled);
+  log.output("会话存储支持(Session Storage)", (sessionStorageEnabled ? "操作成功完成" : "支持但已被禁用，请检查安全上下文"), !sessionStorageEnabled);
 } catch (e) {
   log.output("会话存储支持(Session Storage)", "否", true);
 };
